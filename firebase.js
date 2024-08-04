1c466681448b907b950dc99b947c5ab835448bb4
Dipesh Chaudhary <149223446+Dipesh0310@users.noreply.github.com>
2024-08-04 12:14:55 -0400

    This is my Lab4

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyBwdDdW4rclS90sC8igGOZT5dl0aI5HCyM",
   authDomain: "todo-list-aea66.firebaseapp.com",
@@ -13,10 +13,10 @@ const firebaseConfig = {
   appId: "1:330040594572:web:65c2433d9773135435aba9"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
+
 const db = getFirestore(app);
 
 export { db };
